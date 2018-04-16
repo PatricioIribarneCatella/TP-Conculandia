2517496722cc2255151b04b34f54847e9c8dd66e
PatricioIribarneCatella <patricio_14_94@hotmail.com>
2018-04-15 22:48:24 -0300

    new files added to clang-format and make format them

--- Shell.c
+++ Shell.c
@@ -2,50 +2,46 @@
 
 static char buffer[BUFLEN];
 
-static char* read_line(const char* promt) {
-	
-	int i = 0,
-            c = 0;
+static char *read_line(const char *promt) {
+	int i = 0, c = 0;
 
-        memset(buffer, 0, BUFLEN);
+	memset(buffer, 0, BUFLEN);
 
-        fprintf(stdout, "%s", promt);
+	fprintf(stdout, "%s", promt);
 
-        c = getchar();
+	c = getchar();
 
-        while (c != END_LINE && c != EOF) {
-                buffer[i++] = c;
-                c = getchar();
-        }
+	while (c != END_LINE && c != EOF) {
+		buffer[i++] = c;
+		c = getchar();
+	}
 
-        if (c == EOF)
-                return NULL;
+	if (c == EOF)
+		return NULL;
 
-        buffer[i] = END_STRING;
+	buffer[i] = END_STRING;
 
-        return buffer;
+	return buffer;
 }
 
-void Shell_run(pid_t f, Log* log, Contador* p) {
-
-	char* cmd;
+void Shell_run(pid_t f, Log *log, Contador *p) {
+	char *cmd;
 
 	printf(
 		"\tConculandia - Shell\n"
 		" Comandos:\n"
 		"\texit: terminar la simulación\n"
-		"\tgetper: devuelve la cantidad de personas que llegaron a la Frontera\n");
+		"\tgetper: devuelve la cantidad de personas que llegaron a la "
+		"Frontera\n");
 
 	while ((cmd = read_line(">")) != NULL) {
-		
 		if (strcmp(cmd, "exit") == 0) {
-
 			Log_escribir(log, "KILL FRONTERA\n");
 
 			// Signal a la frontera para
 			// terminar de producir gente
 			kill(f, SIGINT);
-			
+
 			// Se espera el término
 			// del proceso Frontera
 			waitpid(f, NULL, 0);
@@ -56,7 +52,7 @@ void Shell_run(pid_t f, Log* log, Contador* p) {
 		}
 
 		if (strcmp(cmd, "getper") == 0)
-			printf("Personas que llegaron a Conculandia: %d\n", Contador_get(p));
+			printf("Personas que llegaron a Conculandia: %d\n",
+				   Contador_get(p));
 	}
 }
-
--- Shell.h
+++ Shell.h
@@ -3,8 +3,8 @@
 
 #include <stdio.h>
 #include <string.h>
-#include <sys/wait.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 
 #include "Contador.h"
 #include "Log.h"
@@ -13,6 +13,6 @@
 #define END_LINE '\n'
 #define END_STRING '\0'
 
-void Shell_run(int f, Log* log, Contador* p);
+void Shell_run(int f, Log *log, Contador *p);
 
-#endif // SHELL_H
+#endif  // SHELL_H
